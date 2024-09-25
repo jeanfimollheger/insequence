1c03e86f99a323f721ddb727319dfa7f630b24c0
jeanfimollheger <jpmh@gmx.fr>
2024-09-25 14:01:51 +0200

    creation model SiteUserSignupForm & admin.site.register model SiteUser

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,8 @@
 from django import forms
-
+from accounts.models import SiteUser
+class SiteUserSignupForm(forms.ModelForm):
+  class Meta:
+    model = SiteUser
+    fields = ['username', 'first_name', 'last_name', 'email', 'password']
+    labels = {'username':'Pseudo', 'first_name':'Prénom', 'last_name':'Nom', 'email':'email', 'password':'Mot de passe'}
+    widgets = {'password':forms.PasswordInput()}
